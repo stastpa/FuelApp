77b9e9a8fee1759c90e9b5d389557923858432cb
Patrik Stastny <stastpa8@fit.cvut.cz>
2023-11-01 16:37:37 +0100

    Initial commit

---
+++ fuelapp/src/main/java/cz/cvut/fit/tjv/fuelapp/FuelappApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.fuelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelappApplication.class, args);
+	}
+
+}
---
+++ fuelapp/src/test/java/cz/cvut/fit/tjv/fuelapp/FuelappApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.fuelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
