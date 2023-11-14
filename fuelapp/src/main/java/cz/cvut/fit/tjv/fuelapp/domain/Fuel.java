51eca04a432feedde536835321d20112a4c668d8
Patrik Stastny <stastpa8@fit.cvut.cz>
2023-11-14 11:08:21 +0100

    domains created

---
+++ fuelapp/src/main/java/cz/cvut/fit/tjv/fuelapp/domain/Fuel.java
@@ -0,0 +1,24 @@
+package cz.cvut.fit.tjv.fuelapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Fuel")
+public class Fuel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_fuel")
+    Long id;
+    @Column(name = "name")
+    String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = GasStation.class)
+    @JoinColumn(name = "soldAtGasStation")
+    GasStation soldAtGasStation;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "fuelRecords", targetEntity = Record.class, orphanRemoval = true)
+    @JoinColumn(name = "fuelRecords")
+    List<Record> fuelRecords;
+}
